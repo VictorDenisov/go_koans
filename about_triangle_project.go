18e7e40d13d170ba30d2783934d376fd1e39b14a
VictorDenisov <denisovenator@gmail.com>
2011-01-23 14:17:39 +0300

    AssertTrue is replaces with AssertEquals

--- about_triangle_project.go
+++ about_triangle_project.go
@@ -8,29 +8,29 @@ import "./triangle"
 func TestEquilateralTrianglesHaveEqualSides(t *koans.T) {
 	var result string
 	result, _ = triangle.Triangle(2, 2, 2)
-	t.AssertTrue("equilateral" == result)
+	t.AssertEquals("equilateral", result)
 	result, _ = triangle.Triangle(10, 10, 10)
-	t.AssertTrue("equilateral" == result)
+	t.AssertEquals("equilateral", result)
 }
 
 func TestIsoscelesTrianglesHaveExactlyTwoSidesEqual(t *koans.T) {
 	var result string
 	result, _ = triangle.Triangle(3, 4, 4)
-	t.AssertTrue("isosceles" == result)
+	t.AssertEquals("isosceles", result)
 	result, _ = triangle.Triangle(4, 3, 4)
-	t.AssertTrue("isosceles" == result)
+	t.AssertEquals("isosceles", result)
 	result, _ = triangle.Triangle(4, 4, 3)
-	t.AssertTrue("isosceles" == result)
+	t.AssertEquals("isosceles", result)
 	result, _ = triangle.Triangle(10, 10, 2)
-	t.AssertTrue("isosceles" == result)
+	t.AssertEquals("isosceles", result)
 }
 
 func TestScaleneTrianglesHaveNoEqualSides(t *koans.T) {
 	var result string
 	result, _ = triangle.Triangle(3, 4, 5)
-	t.AssertTrue("scalene" == result)
+	t.AssertEquals("scalene", result)
 	result, _ = triangle.Triangle(10, 11, 12)
-	t.AssertTrue("scalene" == result)
+	t.AssertEquals("scalene", result)
 	result, _ = triangle.Triangle(5, 4, 2)
-	t.AssertTrue("scalene" == result)
+	t.AssertEquals("scalene", result)
 }
